6af7c5730d52e1ce88832274c7106c11ef93ecdf
Boris Scheiman <bscheiman@gmail.com>
2014-09-09 14:55:54 -0500

    Components
    ServiceStack, ModernHttpClient & AcrUserDialogs

--- Pages/BasePage.cs
+++ Pages/BasePage.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using Xamarin.Forms;
 
 namespace App.Pages {
-	public class BasePage {
+	public class BasePage : ContentPage {
+		public BasePage() {
+			// iOS 7 Status bar
+			Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5);
+		}
 	}
 }
 
